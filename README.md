ef014ed2b4a6461abfa050ad46dd58aab27abc16
Justin Stitt <justinstitt@google.com>
2024-12-26 22:10:51 -0800

    remove old files, prep for sveltekit transition
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- README.md
+++
@@ -1,5 +0,0 @@
-# jstitt.dev
-
-It's gonna be a meme.
-
--=WIP=-
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/app.css'
-import './assets/kanagawa.css'
-import App from './App.svelte'
-
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
