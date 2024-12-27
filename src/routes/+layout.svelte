a90f8274fac9f6c66e2c271a8fd52a2fc02b0f0f
Justin Stitt <justinstitt@google.com>
2024-12-26 22:58:38 -0800

    add layout navbar
    Signed-off-by: Justin Stitt <justinstitt@google.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,13 @@
 	let { children } = $props();
 </script>
 
+<div class="flex h-14 items-center justify-between px-4 lg:px-6">
+	<h1 class="font-mono text-3xl font-bold">JS</h1>
+	<h1 class="font-mono text-3xl font-bold">jstitt.dev</h1>
+	<div class="flex gap-4">
+		<a class="text-blue-500 underline" href="https://github.com/justinstitt/jstitt.dev">repo</a>
+		<h1>idk</h1>
+	</div>
+</div>
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <div class="flex flex-col items-center justify-center">
 	<h1>In development, come back later :)</h1>
-	<a class="text-blue-500 underline" href="https://github.com/justinstitt/jstitt.dev">repo</a>
 </div>
